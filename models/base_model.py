bcfa5c0fb70f11699f6d07b6575e48f23df9a400
Leac1m <obemichael10@gmail.com>
2023-05-15 21:13:32 +0100

    commited

--- models/base_model.py
+++ models/base_model.py
@@ -13,18 +13,21 @@ class BaseModel:
         """
         Constructor method for BaseModel.
 
-        Initializes the object with provided attributes or generates default values.
+        Initializes the object with provided
+        attributes or generates default values.
         """
         if not kwargs:
             # If no keyword arguments are provided, generate default values
-            self.id = str(uuid4())  # Generate a unique identifier using uuid4()
-            self.created_at = self.updated_at = datetime.now()  # Set creation and update timestamps
+            self.id = str(uuid4())
+            self.created_at = self.updated_at = datetime.now()
         else:
-            # If keyword arguments are provided, set the corresponding attributes
+            # If keyword arguments are provided,
+            # set the corresponding attributes
             for k, v in kwargs.items():
                 if k != "__class__":
                     if k in ("created_at", "updated_at"):
-                        # If the attribute is a timestamp, convert it from ISO format to datetime object
+                        # If the attribute is a timestamp, convert
+                        # it from ISO format to datetime object
                         setattr(self, k, datetime.fromisoformat(v))
                     else:
                         # Set the attribute with the provided value
@@ -35,9 +38,11 @@ class BaseModel:
         Return a string representation of the object.
 
         Returns:
-            str: A formatted string containing the class name, id, and attribute dictionary.
+            str: A formatted string containing the class name,
+            id, and attribute dictionary.
         """
-        return "[{}] ({}) {}".format(__class__.__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(__class__.__name__,
+                                     self.id, self.__dict__)
 
     def save(self):
         """
@@ -50,14 +55,14 @@ class BaseModel:
         Convert the object to a dictionary representation.
 
         Returns:
-            dict: A dictionary containing the object's attributes and class name.
+            dict: A dictionary containing the
+            object's attributes and class name.
         """
-        obj = self.__dict__.copy()  # Create a copy of the attribute dictionary
-        obj["__class__"] = __class__.__name__  # Add the class name to the dictionary
+        obj = self.__dict__.copy()
+        obj["__class__"] = __class__.__name__
 
         for k, v in obj.items():
             if k in ("created_at", "updated_at"):
-                # If the attribute is a timestamp, convert it to ISO format string
                 obj[k] = datetime.isoformat(v)
 
         return obj
