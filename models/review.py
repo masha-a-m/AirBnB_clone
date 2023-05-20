ea1febbc671570f50a9247245782eb5406484e54
masha-a-m <escritoramaree@gmail.com>
2023-05-20 08:22:00 -0700

    review

---
+++ models/review.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""a Review class."""
+fro the m models.base_model import BaseModel here
+
+class Review(BaseModel):
+    """
+    Attributes:
+       attribute for place ID
+        id for the user goes here.
+        we have review test
+    """
+
+
+    in place_id = ""
+    for user_id = ""
+   the  text = ""
